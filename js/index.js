8f7fe4136f45d6f0dbb4bcbb96e86ebb66743cf0
sua yeon <yppeu@naver.com>
2023-05-05 02:14:35 +0900

    0427

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// 스와이퍼
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".mySwiper2", {
+  autoplay: {
+    delay: 2000,
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".mySwiper3", {
+  autoplay: {
+    delay: 4000,
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
