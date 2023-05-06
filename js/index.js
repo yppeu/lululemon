3fd18a3dafc70e7d0376b3f86a8bc74f8020a913
sua yeon <yppeu@naver.com>
2023-05-06 23:53:55 +0900

    0506

--- js/index.js
+++ js/index.js
@@ -33,6 +33,25 @@ var swiper = new Swiper(".mySwiper3", {
   },
 });
 var swiper = new Swiper(".mySwiper4", {
-  watchSlidesProgress: true,
+  autoplay: {
+    delay: 4000,
+  },
   slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// 헤더 스크롤 시 백그라운드 생성
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $("header").addClass("active");
+    } else {
+      $("header").removeClass("active");
+    }
+  });
 });
